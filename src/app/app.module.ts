20488ac96d3e9972ba8aa6a64bb6122119f4fe46
Retrojb <baltescartography@gmail.com>
2020-01-23 16:19:48 -0500

    adding containers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { appReducers } from './store/reducers/app.reducer';
+import { HouseEffects } from './store/effects/house.effect';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -5,35 +7,33 @@ import { RoomService, HouseService } from './services';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './store/reducers';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { SocketDriver } from 'sdk-v1/sockets/socket.driver';
 import { HttpClientModule } from '@angular/common/http';
 import { SocketConnection } from 'sdk-v1/sockets/socket.connections';
 import { SDKModels, LoopBackAuth, ErrorHandler, InternalStorage } from 'sdk-v1';
+import { EffectsModule } from '@ngrx/effects';
+import { RoomEffects } from './store/effects/room.effect';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { HouseComponent } from './containers/house/house.component';
+import { RoomsComponent } from './containers/rooms/rooms.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HouseComponent,
+    RoomsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot(reducers, {
-      metaReducers,
-      runtimeChecks: {
-        strictStateImmutability: true,
-        strictActionImmutability: true,
-      }
-    }),
+    StoreModule.forRoot(appReducers),
+    EffectsModule.forRoot([
+      HouseEffects,
+      RoomEffects
+    ]),
+    StoreRouterConnectingModule.forRoot({ stateKey: 'router' }),
     !environment.production ? StoreDevtoolsModule.instrument() : [],
-    StoreModule.forRoot(reducers, {
-      metaReducers,
-      runtimeChecks: {
-        strictStateImmutability: true,
-        strictActionImmutability: true,
-      }
-    }),
   ],
   providers: [
     HouseService,
--- src/app/store/reducers/house.reducer.spec.ts
+++ src/app/store/reducers/house.reducer.spec.ts
@@ -1,13 +1,13 @@
-import { reducer, initialState } from './house.reducer';
+import { reducer, initialHouseState } from './house.reducer';
 
 describe('House Reducer', () => {
   describe('an unknown action', () => {
     it('should return the previous state', () => {
       const action = {} as any;
 
-      const result = reducer(initialState, action);
+      const result = reducer(initialHouseState, action);
 
-      expect(result).toBe(initialState);
+      expect(result).toBe(initialHouseState);
     });
   });
 });
